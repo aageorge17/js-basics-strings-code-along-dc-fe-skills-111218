c4556f5c39804687683feedef499ce5a5b20edb5
Andrea George <aageorge17@gmail.com>
2019-01-07 22:45:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const currentUser = 'John Doe';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = 'Welcome, ' +
+const shortGreeting = 'Welcome, ' + currentUser(0); 
 // Write your code in this file!

bb2cae9cc257b04bf40c5d588b34cb2580e0f58b
Andrea George <aageorge17@gmail.com>
2019-01-07 22:43:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = 'John Doe';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 // Write your code in this file!
